be9395cf49657331e307a3861472cc719f230b84
Ray <raitis.cerkasovs@gmail.com>
2020-06-29 09:08:30 +0100

    update Betfair url to eventTimelines

--- controllers/mainApiController.js
+++ controllers/mainApiController.js
@@ -2,11 +2,11 @@ var config = require("../config/config");
 var request = require("request");
 
 function listEventStatusUrl(apiKey, eventId) { 
-    return "https://ips.betfair.com/inplayservice/v1/scores"
+    return "https://ips.betfair.com/inplayservice/v1/eventTimelines"
     + "?_ak=" + apiKey
     + "&alt=json"
     + "&eventIds=" + eventId 
-    + "&locale=en"
+    + "&locale=en_GB"
 }
 
 module.exports = { run: function(app) { 
